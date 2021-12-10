12abadb1d9846fb0d213c5725803c9ce1399a538
Andrew Schmelyun <schmelyun@gmail.com>
2021-12-10 03:30:55 -0500

    Adds changes for Chapter 4

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,13 +1,35 @@
 <template>
-    <div>
-        <h2 class="text-xl text-center">{{ message }}</h2>
+    <div class="mt-4">
+        <file-pond
+            name="image"
+            ref="pond"
+            label-idle="Click to choose image, or drag here..."
+            server="/upload"
+            @init="filepondInitialized"
+            accepted-file-types="image/*"
+        />
     </div>
 </template>
 <script>
+import vueFilePond from 'vue-filepond';
+import "filepond/dist/filepond.min.css";
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'; 
+
+const FilePond = vueFilePond(FilePondPluginFileValidateType);
+
 export default {
+    components: {
+        FilePond
+    },
     data() {
         return {
-            message: 'Hello, world!'
+
+        }
+    },
+    methods: {
+        filepondInitialized() {
+            console.log('Filepond is ready!');
+            console.log('Filepond object:', this.$refs.pond);
         }
     }
 }
